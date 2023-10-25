430f7999106ada1b125ae1db520444184078b667
NatáliaFonseca <fonsecanatalianf@gmail.com>
2023-10-25 15:58:17 -0300

    implementing tests

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,27 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve verificar se há dois comentarios', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('textarea-comentario'),{
+            target: {
+                value:'Comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('button-comentario'));
+        fireEvent.change(screen.getByTestId('textarea-comentario'),{
+            target: {
+                value:'Segundo comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('button-comentario'));
+
+        expect(screen.getAllByTestId('campo-comentarios')).toHaveLength(2);
     });
 });
